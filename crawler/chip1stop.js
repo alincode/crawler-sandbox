a3070f260d21776a3921d1db45bafeba33b79db6
艾霖 劉 <ailinliu@ailindeMacBook-Pro.local>
2017-01-02 19:29:43 +0800

    chip1stop

---
+++ crawler/chip1stop.js
@@ -0,0 +1,39 @@
+var ROOT_URL = 'http://www.chip1stop.com/';
+
+var Crawler = require('simplecrawler');
+var crawler = new Crawler(ROOT_URL);
+var utils = require('utility');
+var ApiService = require('../services/ApiService');
+
+crawler.interval = 30 * 1000;
+crawler.maxConcurrency = 1;
+crawler.decodeResponses = true;
+crawler.urlEncoding = 'utf8';
+
+crawler.on('crawlstart', function() {
+  console.log('Crawl starting ', ROOT_URL);
+});
+
+crawler.addFetchCondition(function(queueItem, referrerQueueItem) {
+  return queueItem.path.indexOf('search\.do') > -1;
+});
+
+crawler.addFetchCondition(function(queueItem, referrerQueueItem) {
+  return queueItem.path.indexOf('dispDetail\.do') > -1 && queueItem.path.indexOf(
+    'otherZaikoDispFlg') == -1
+});
+
+crawler.on('fetchcomplete', function(queueItem, responseBuffer, response) {
+  console.log('=== fetchcomplete === url:', queueItem.url);
+  if (queueItem.path.indexOf('dispDetail') == -1) return;
+  if (ROOT_URL == queueItem.url) return;
+  var url = queueItem.url;
+  var html = utils.base64encode(responseBuffer.toString());
+  ApiService.save(url, html);
+});
+
+crawler.on('complete', function() {
+  console.log('Finished!', ROOT_URL);
+});
+
+crawler.start();
