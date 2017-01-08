af774d1822bd4254d78bbec634d5d10caa9034de
alincode <alincode@gmail.com>
2017-01-09 01:44:37 +0800

    update example

--- phantom_test.js
+++ phantom_test.js
@@ -1,2 +1,14 @@
-console.log('Hello, world!');
-phantom.exit();
+console.log('Loading a web page');
+var page = require('webpage').create();
+var url =
+  'http://www.digikey.com.cn/search/zh/VLP-500-F/492-1517-ND?recordId=3091702';
+
+// page.open(url, function(status) {
+//   console.log(status);
+//   phantom.exit();
+// });
+
+page.open(url, function(status) {
+  console.log('Stripped down page text:\n' + page.plainText);
+  phantom.exit();
+});
